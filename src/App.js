a184399e6ddbd9683f154b56d187077a885286d0
unknown <costa.sergio.86@gmail.com>
2021-03-09 23:26:52 +0000

    Using .Map to Display Data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import './App.css';
 import PortfolioItem from './components/PortfolioItem'
 
+const data = [
+  {
+      "id": 1,
+      "title": "Everything is gold",
+      "description": "Life is Gold.",
+      "background": "#ffd700",
+      "published_at": "2021-03-09T21:57:39.021Z",
+      "created_at": "2021-03-09T21:52:26.940Z",
+      "updated_at": "2021-03-09T22:03:50.442Z"
+  },
+  {
+      "id": 2,
+      "title": "Red is passion",
+      "description": "Red is fire",
+      "background": " #ff0000",
+      "published_at": "2021-03-09T21:57:44.801Z",
+      "created_at": "2021-03-09T21:53:42.878Z",
+      "updated_at": "2021-03-09T22:03:57.890Z"
+  }
+]
+
 function App() {
   return (
     <div className="App">
-      <PortfolioItem />
-      <PortfolioItem />
-      <PortfolioItem />
+      {
+        data.map(entry => (
+          <PortfolioItem title={entry.title} description={entry.description}/>
+        ))
+      }
     </div>
   );
 }
--- src/components/PortfolioItem.js
+++ src/components/PortfolioItem.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
-const PortfolioItem = () => {
+const PortfolioItem = ({title, description}) => {
     return (
         <div className='PortfolioItem'>
-            PortfolioItem
+            <h2>{title}</h2>
+            <p>{description}</p>
         </div>
     )
 }
