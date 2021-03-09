a3ceeeb8a10c5f735ada9b059ed5314160a016f9
unknown <costa.sergio.86@gmail.com>
2021-03-09 23:03:11 +0000

    Creating the Portfolio Item Component

---
+++ src/components/PortfolioItem.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PortfolioItem = () => {
+    return (
+        <div className='PortfolioItem'>
+            PortfolioItem
+        </div>
+    )
+}
+
+export default PortfolioItem
